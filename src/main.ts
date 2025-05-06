38c8b80e035de8d7b4e8138eeef67fa396bd04f9
Jackie <kanakanajm@outlook.com>
2025-05-06 09:28:32 +0200

    Add cursor zoom but it has a slight shift after cursor change position

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,7 @@ instance().then(viz => {
     .attr("cy", cy)
     .attr("r", 5)
     .attr("fill", "red")
+  console.log("Centroid: ", cx, cy);
 
   const translation = gElement.transform.baseVal.getItem(2);
   if (translation.type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
@@ -43,9 +44,49 @@ instance().then(viz => {
   const tx = translation.matrix.e;
   const ty = translation.matrix.f;
 
-  const graphZoomBehavior = d3.zoom<SVGSVGElement, unknown>().on("zoom", (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
-    graph.attr("transform", `translate(${tx} ${ty}) translate(${cx} ${cy}) scale(${event.transform.k}) translate(${-cx} ${-cy})`);
+  const svg = d3.select<SVGSVGElement, unknown>("svg");
+  const svgElement = svg.node()!;
+
+  // const toLocalTransform = gElement.getScreenCTM()!.inverse()
+
+  const graphZoomBehavior = d3.zoom<SVGSVGElement, unknown>()
+    .on("zoom", (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
+    // Create an SVGPoint to hold screen coordinates
+
+    const worldToLocal = gElement.getScreenCTM()!.inverse();
+
+    const pt = svgElement.createSVGPoint();
+    pt.x = event.sourceEvent.clientX;
+    pt.y = event.sourceEvent.clientY;
+    const cursorpt =  pt.matrixTransform(worldToLocal);
+
+    console.log("Zoom at", cursorpt);
+
+    // plot zoomed point
+  // graph.append("circle")
+  // .attr("class", "zoomed-point")
+  // .attr("cx", p.x)
+  // .attr("cy",  p.y)
+  // .attr("r", 5)
+  // .attr("fill", "blue")
+
+    graph.attr("transform", `translate(${tx} ${ty}) translate(${cursorpt.x} ${cursorpt.y}) scale(${event.transform.k}) translate(${-cursorpt.x} ${-cursorpt.y})`);
   });
   
-  d3.select<SVGSVGElement, unknown>("svg").call(graphZoomBehavior);
+  svg.call(graphZoomBehavior);
+
+  svg.on("click", evt => {
+    const worldToLocal = gElement.getScreenCTM()!.inverse();
+
+    const pt = svgElement.createSVGPoint();
+    pt.x = evt.clientX;
+    pt.y = evt.clientY;
+    const cursorpt =  pt.matrixTransform(worldToLocal);
+    graph.append("circle")
+    .attr("class", "cursor-point")
+    .attr("cx", cursorpt.x)
+    .attr("cy", cursorpt.y)
+    .attr("r", 5)
+    .attr("fill", "blue");
+  });
 });
